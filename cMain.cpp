6d4ecbaf2899eaea5b76e6c7d63c0aafdea8a1ec
JHilario97 <jesus.hilario1997@gmail.com>
2022-07-11 17:29:10 -0400

    finished

--- cMain.cpp
+++ cMain.cpp
@@ -1,7 +1,7 @@
 #include "cMain.h"
 
 
-cMain::cMain() : wxFrame(nullptr, wxID_ANY, "Calcoolator B)", wxPoint(30,30), wxSize(500,500))
+cMain::cMain() : wxFrame(nullptr, wxID_ANY, "Calcoolator B)", wxPoint(30,30), wxSize(500,460))
 {
 	m_btn1 = new wxButton(this, wxID_ANY, "1", wxPoint(0, 50), wxSize(91, 75));
 	m_btn1 = new wxButton(this, wxID_ANY, "2", wxPoint(91, 50), wxSize(91, 75));
@@ -14,8 +14,17 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "Calcoolator B)", wxPoint(30,30), wx
 	m_btn1 = new wxButton(this, wxID_ANY, "9", wxPoint(182, 200), wxSize(91, 75));
 	m_btn1 = new wxButton(this, wxID_ANY, "0", wxPoint(91, 275), wxSize(91, 75));
 
+	m_btn1 = new wxButton(this, wxID_ANY, "+", wxPoint(182, 275), wxSize(91, 75));
+	m_btn1 = new wxButton(this, wxID_ANY, "-", wxPoint(91, 350), wxSize(91, 75));
+	m_btn1 = new wxButton(this, wxID_ANY, "x", wxPoint(182, 350), wxSize(91, 75));
+	m_btn1 = new wxButton(this, wxID_ANY, "/", wxPoint(0, 350), wxSize(91, 75));
+
+	m_btn1 = new wxButton(this, wxID_ANY, "C", wxPoint(0, 275), wxSize(91, 75));
+	m_btn1 = new wxButton(this, wxID_ANY, "8", wxPoint(91, 200), wxSize(91, 75));
+	
+
 	m_txt1 = new wxTextCtrl(this, wxID_ANY, "", wxPoint(0, 0), wxSize(275, 50));
-	m_list1 = new wxListBox(this, wxID_ANY, wxPoint(275, 0), wxSize(210, 500));
+	m_list1 = new wxListBox(this, wxID_ANY, wxPoint(275, 0), wxSize(210, 425));
 }
 
 cMain::~cMain()
